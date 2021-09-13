63c7f1c406b39833413f988fab1e61b26e752336
Mihirss <mihirsoniTM@gmail.com>
2021-09-13 16:30:26 +0530

    Todo in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
+
+
+
+const App = () => {
+  const [todoList,setTodoList] = useState([])// setNewtodoList. todoList, setTodoList
+  const [taskName, setTaskname] = useState('')
+
+  
+  function handleSubmit(e){
+    if(taskName != 0){
+    e.preventDefault();
+    setTodoList([...todoList,{id: todoList.length, name: taskName}]);
+    setTaskname("");
+    }
+    else{
+      alert("To-Do List need Some Task for execute. so, Please Must be add Some Task.")
+    }
+  }
+
+
+
+  function removeItem(e){
+    console.log(e)
+    e.target.parentElement.remove()
+  }
+
+  function check (e) {
+    console.log(e)
+    if(e.target.className==='')
+    {
+      e.target.classList.toggle('checked')
+    }
+    else{
+      e.target.classList.remove('checked')
+    }
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <>
+      <form>
+      <Typography component="h1" variant="h5">Task</Typography>
+      <TextField type="text" placeholder="Task here..." value={taskName} onChange={e => setTaskname(e.target.value)} />
+      <Button variant="contained" color="primary" onClick = {handleSubmit}>Add To my List</Button>
+      </form>
+      <Typography component="h1" variant="h5">To-Do List</Typography>
+      <ul>
+        {todoList.map(item => (
+          <li onClick={check} key ={item.id}>{item.name} <button onClick={removeItem}>Delete</button> </li>
+        ))}
+      </ul>
+      </>
+  )
 }
 
-export default App;
+export default App
