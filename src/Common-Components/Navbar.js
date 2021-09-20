8a09d4389e0e4d11d170be4e023eb925ee6a8ffb
Mihirss <mihirsoniTM@gmail.com>
2021-09-20 16:54:52 +0530

    Todo list with navbar nad feed

--- src/Common-Components/Navbar.js
+++ src/Common-Components/Navbar.js
@@ -8,23 +8,18 @@ const Navbar = () => {
             <div className="container-fluid nav_bg">
                 <div className="row">
                     <div className="col-10 mx-auto">
-                        <nav className="navbar navbar-expand-lg bg-transparent">
+                        <nav className="navbar navbar-light bg-transparent">
                             <div className="container-fluid">
                                 <NavLink className="navbar-brand" to="/">Mr.Xmen</NavLink>
-                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
-                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                                        <li className="nav-item">
-                                            <NavLink exact activeClassName="Active-nav" className="nav-link" to="/">Home</NavLink>
+                                    <ul id="navbar" className="navbar-nav">
+                                        <li  className="nav-item">
+                                            <NavLink exact className="nav-link" to="/">Home</NavLink>
                                         </li>
                                         <li className="nav-item">
-                                            <NavLink exact activeClassName="Active-nav" className="nav-link" to="/Feed">Feed</NavLink>
+                                            <NavLink exact className="nav-link" to="/Feed">Feed</NavLink>
                                         </li>
                                     </ul>
                                 </div>
-                            </div>
                         </nav>
                     </div>
                 </div>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-function Card({item}) {
+function Card({ item }) {
     return (
-        <div>
             <div className="card">
-            <img src={item.avatar} className="card-img-top" alt="..."/>
-            <div className ="card-body">
-            <h5 className ="card-title">Name: {item.first_name}</h5>
-            <p className ="card-text">Email:{item.email}</p>
-            {/* <a href="#" className ="btn btn-primary">Go somewhere</a> */}
+                <div className="card-columns">
+                    <img src={item.avatar} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Name: {item.first_name}</h5>
+                        <p className="card-text">Email:{item.email}</p>
+                        {/* <a href="#" className ="btn btn-primary">Go somewhere</a> */}
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
     )
 }
 
